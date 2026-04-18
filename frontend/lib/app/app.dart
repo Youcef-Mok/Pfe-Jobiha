0d29d89fce80c246f7f0393c659e40d980f65328
Zemour.F <zemourferiel7@gmail.com>
2026-04-18 22:17:49 +0100

    feat: condidate notification complete

--- frontend/lib/app/app.dart
+++ frontend/lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:job_app/features/auth/screens/welcome_screen.dart';
 import 'package:job_app/features/auth/screens/login_screen.dart';
 import 'package:job_app/features/auth/screens/signup_role_screen.dart';
@@ -9,26 +10,32 @@ import 'package:job_app/features/auth/screens/preferences_screen.dart';
 import 'package:job_app/features/jobs/screens/jobs_list_screen.dart';
 import 'package:job_app/features/messaging/screens/chat_list_screen.dart';
 import 'package:job_app/features/messaging/screens/chat_screen.dart';
+import 'package:job_app/features/notifications/screens/candidate_notifications_screen.dart';
+import 'package:job_app/features/notifications/screens/notifications_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/messages',
-      routes: {
-        '/welcome':           (context) => const WelcomeScreen(),
-        '/login':             (context) => const LoginScreen(),
-        '/signup':            (context) => const SignupRoleScreen(),
-        '/signup-form':       (context) => const SignupFormScreen(),
-        '/signup-profile':    (context) => const SignupProfileScreen(),
-        '/recruiter-profile': (context) => const RecruiterProfileScreen(),
-        '/preferences':       (context) => const PreferencesScreen(),
-        '/jobs':              (context) => const JobsListScreen(),
-        '/messages':          (context) => const ChatListScreen(),
-      },
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/candidate-notifications',
+        routes: {
+          '/welcome':                    (context) => const WelcomeScreen(),
+          '/login':                      (context) => const LoginScreen(),
+          '/signup':                     (context) => const SignupRoleScreen(),
+          '/signup-form':                (context) => const SignupFormScreen(),
+          '/signup-profile':             (context) => const SignupProfileScreen(),
+          '/recruiter-profile':          (context) => const RecruiterProfileScreen(),
+          '/preferences':                (context) => const PreferencesScreen(),
+          '/jobs':                       (context) => const JobsListScreen(),
+          '/messages':                   (context) => const ChatListScreen(),
+          '/candidate-notifications':    (context) => const CandidateNotificationsScreen(),
+          '/recruiter-notifications':    (context) => const NotificationsScreen(),
+        },
+      ),
     );
   }
 }
